be684f7078c91c763a14f97b5ed940661e05290c
liz <lizssky@gmail.com>
2022-11-22 18:17:57 +0800

    fixbug

--- signPdf/src/components/MoveSign.vue
+++ signPdf/src/components/MoveSign.vue
@@ -7,45 +7,47 @@
 </template>
 
 <script setup lang="ts">
-import { nextTick, onMounted, ref } from 'vue'
+import { nextTick, onMounted, ref, watch } from 'vue'
 import { fabric } from 'fabric';
 import common from '../stores/common';
 import { storeToRefs } from "pinia";
+import { computed } from '@vue/reactivity';
 const commonStore = common();
 
 const props = defineProps({
   wC:Number,
   HC:Number,
 })
+const { singImg } = storeToRefs(commonStore);
 
 
-onMounted(()=>{
-  nextTick(()=>{
-   
-        const card = new fabric.Canvas('canvas',{
-          width:screen.width,
+watch(()=>singImg.value,()=>{
+  getImg();
+})
+
+
+const getImg = ()=>{
+  const card = new fabric.Canvas('canvas',{
+          width:props.wC,
           height:props.HC,
         }) 
-  
 
-    
-
-      const { singImg } = commonStore;
      
-      
       if(singImg){
-        fabric.Image.fromURL(singImg, oImg => {
+        fabric.Image.fromURL(singImg.value, oImg => {
       // 将图片对象添加到 canvas 中
         card.add(oImg)
       })
 
-
       }
+  
+}
 
-    
 
-      card.add(circle)
-      
+
+onMounted(()=>{
+  nextTick(()=>{
+    getImg();
   })
   
 })
